a97988d04c2b50d0f4d95413eb0c70b6d91b6579
Merlot <merlotrain@gmail.com>
2024-01-08 00:10:17 +0800

    add color wheel

--- editor/colorwidgets/colorwheel.cpp
+++ editor/colorwidgets/colorwheel.cpp
@@ -1,4 +1,6 @@
 #include "colorwheel.h"
+#include <QConicalGradient>
+#include <QPainter>
 
 class ColorWheelPrivate
 {
@@ -32,13 +34,79 @@ public:
 
 /* ------------------------------- ColorWheel ------------------------------- */
 
-ColorWheel::ColorWheel(QWidget *parent) {}
+ColorWheel::ColorWheel(QWidget *parent)
+    : ColorWidget(parent), d(new ColorWheelPrivate(this))
+{
+    QConicalGradient wheelGradient = QConicalGradient(0, 0, 0);
+    const int wheelStops = 20l;
+    QColor gradColor = QColor::fromHsvF(1., 1., 1.);
+    for (int pos = 0; pos < wheelStops; ++pos)
+    {
+        const double relativePos = static_cast<double>(pos) / wheelStops;
+        gradColor.setHsvF(relativePos, 1., 1.);
+        wheelGradient.setColorAt(relativePos, gradColor);
+    }
+    d->wheelBrush = QBrush(wheelGradient);
+}
 
 ColorWheel::~ColorWheel() {}
 
-QSize ColorWheel::sizeHint() const { return QSize(); }
+QSize ColorWheel::sizeHint() const
+{
+    const int size = fontMetrics().horizontalAdvance('X') * 22;
+    return QSize(size, size);
+}
 
-void ColorWheel::paintEvent(QPaintEvent *event) {}
+void ColorWheel::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event)
+    QPainter painter(this);
+
+    if (d->widgetImage.isNull() || d->wheelImage.isNull() || d->triangleImage.isNull())
+    {
+        d->createImage(size());
+    }
+
+    d->widgetImage.fill(Qt::transparent);
+    QPainter imagePainter(&d->widgetImage);
+    imagePainter.setRenderHint(QPainter::Antialiasing);
+    if (d->wheelDirty)
+    {
+        d->createWheel();
+    }
+
+    // draw wheel centered on widget
+    const QPointF center =
+            QPointF(d->widgetImage.width() / 2.0, d->widgetImage.height() / 2.0);
+    imagePainter.drawImage(QPointF(center.x() - (d->wheelImage.width() / 2.0),
+                                   center.y() - (d->wheelImage.height() / 2.0)),
+                           d->wheelImage);
+
+    // draw hue marker
+    const int h = hue();
+    const double length = d->wheelImage.width() / 2.0;
+    QLineF hueMarkerLine =
+            QLineF(center.x(), center.y(), center.x() + length, center.y());
+    hueMarkerLine.setAngle(h);
+    imagePainter.save();
+
+    imagePainter.setCompositionMode(QPainter::CompositionMode_SourceIn);
+    QPen pen;
+    pen.setWidthF(2 * devicePixelRatioF());
+    pen.setColor(h > 20 && h < 200 ? Qt::black : Qt::white);
+    imagePainter.setPen(pen);
+    imagePainter.drawLine(hueMarkerLine);
+    imagePainter.restore();
+
+    // draw triangle
+    if (d->triangleDirty)
+    {
+        d->createTriangle();
+    }
+    imagePainter.drawImage(QPointF(center.x() - (d->wheelImage.width() / 2.0),
+                                   center.y() - (d->wheelImage.height() / 2.0)),
+                           d->triangleImage);
+}
 
 void ColorWheel::setColor(const QColor &color, bool emitSignals) {}
 
@@ -52,7 +120,7 @@ void ColorWheel::mouseReleaseEvent(QMouseEvent *event) {}
 
 /* ---------------------------- ColorWheelPrivate --------------------------- */
 
-ColorWheelPrivate::ColorWheelPrivate(ColorWheel *ptr) {}
+ColorWheelPrivate::ColorWheelPrivate(ColorWheel *ptr) : q(ptr) {}
 
 void ColorWheelPrivate::createImage(QSizeF size) {}
 
